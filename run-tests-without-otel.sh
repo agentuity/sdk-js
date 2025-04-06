7b9da858f44b3df31e580acb923a4dc9664ce653
Jeff Haynie <jhaynie@gmail.com>
2025-04-06 16:01:47 -0500

    cleanup

--- run-tests-without-otel.sh
+++
@@ -1,2 +0,0 @@
-export SKIP_OTEL_TESTS=true
-bun test --timeout 5000 test/apis test/logger test/router test/server
--- run-tests.sh
+++
@@ -1,2 +0,0 @@
-export SKIP_OTEL_TESTS=true
-bun test --timeout 5000 --pattern "!(otel)/**/*.test.ts"
