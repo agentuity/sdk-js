373d8b057b56fae1a947c584d9c936f5b1caa974
Jeff Haynie <jhaynie@gmail.com>
2025-09-24 20:49:27 -0500

    add new utils

---
+++ src/utils/stringify.ts
@@ -0,0 +1,17 @@
+/**
+ * Safely stringify an object to JSON, handling circular references
+ * @param obj - The object to stringify
+ * @returns JSON string representation
+ */
+export function safeStringify(obj: unknown): string {
+	const seen = new WeakSet();
+	return JSON.stringify(obj, (_key, value) => {
+		if (typeof value === 'object' && value !== null) {
+			if (seen.has(value)) {
+				return '[Circular]';
+			}
+			seen.add(value);
+		}
+		return value;
+	});
+}
